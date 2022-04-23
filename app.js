fc0f2dbe9536a34ba7c579917f9c60ac86e15365
Matheus <matrodrigues@live.com>
2022-04-23 19:32:21 -0300

    Implement sum function

--- app.js
+++ app.js
@@ -3,24 +3,90 @@ const displayNumber = document.getElementById('displayNumber');
 
 const numericButtons = document.querySelectorAll('.numeric-buttons button');
 const operationsButtons = document.querySelectorAll('.operations-buttons button');
+let firstValue = '';
+let result = '';
+let firstSavedValue = '';
+let sumResult;
+let sumPressed;
 
-handleButtons(numericButtons);
-handleButtons(operationsButtons);
+handleNumericButtons(numericButtons);
+handleOperatorButtons(operationsButtons);
 
-function handleButtons (id){
+
+
+
+
+
+
+function handleNumericButtons (id){
     id.forEach( button => {
         button.addEventListener('click', 
         () => {
-            console.log(button.value);
-            displayNumber.textContent = `${button.value}`
+            //console.log(button.value);
+            firstValue += button.value;
+            displayNumber.textContent = `${firstValue}`
+            
+            console.log(firstValue);
         }
         );
     });
 }
 
+function handleOperatorButtons (id){
+    id.forEach( button => {
+        button.addEventListener('click',
+        () => {
+            if (button.id == 'sum'){
+                console.log(`${button.id}`);  
+                if(result != '' && firstValue != ''){
+                    firstValue = +firstValue;
+                    //operator('sum', firstSavedValue, firstValue);
+                    result = operations.sum(result, firstValue);
+                    firstValue = '';
+                    displayNumber.textContent = `${result}`
+                    sumPressed = false;     
+                } else {
+                    result = +firstValue;
+                    firstValue = '';
+                    sumPressed = true; 
+                }
+            } else if (button.id == 'equal'){
+                    if(sumPressed === true){
+                    firstValue = +firstValue;
+                    result = operations.sum(result, firstValue);
+                    firstValue = '';
+                    displayNumber.textContent = `${result}`
+                    sumPressed = false;   
+                    }
+            } else if (button.value == 'x') {
+                operator('x');
+            } else if (button.value == '/') {
+                operator('/');
+            } else {
+                operator('=');
+            }
+        })
+    })
+}
+
+
+
+
 const operations = {
-    sum (){},
+    sum (firstValue, secondValue){
+        return firstValue+secondValue;
+    },
     subtraction (){},
     multiplication (){},
     division (){},
+    evaluation (){},
 }
+
+// function operator (buttonId, firstValue, secondValue){
+//     if(buttonId == 'sum'){
+//         sumResult = operations.sum(firstValue, secondValue);
+//         console.log(sumResult);
+//         return sumResult;
+//     }
+    
+// }
