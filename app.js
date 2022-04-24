a0368ad4fec7ff7b284ee30746da4a46d74b3a06
Matheus <matrodrigues@live.com>
2022-04-24 10:24:57 -0300

    Improve some minor functionalities

--- app.js
+++ app.js
@@ -30,33 +30,38 @@ function handleOperatorButtons (id){
     id.forEach( button => {
         button.addEventListener('click',
         () => {
-            if (button.id == 'sum'){
-                console.log(`${button.id}`);  
-                if(result != '' && firstValue != ''){
-                    operator('sum');     
-                } else if(result != '' && firstValue == ''){
-                    result = result;
-                    firstValue = '';
-                    operations[0].isPressed = false;
-                } else {
-                    result = +firstValue;
-                    firstValue = '';
-                    operations[0].isPressed = true; 
-                }
-            } else if (button.id == 'equal'){
-                    if(operations[0].isPressed === true){
-                    operator('sum');  
-                    }
-            } else if (button.value == 'x') {
-                operator('x');
-            } else if (button.value == '/') {
-                operator('/');
-            } else {
-                operator('=');
+            switch (button.id){
+                case 'sum':
+                    operations[4].evaluation()
+                    defineOperationLogic('sum', 0, button);
+                break;
+             case 'subtraction':
+                    operations[4].evaluation()
+                    defineOperationLogic('subtraction', 1, button);
+                break;    
+             case 'multiplication':
+                    operations[4].evaluation()
+                    defineOperationLogic('multiplication', 2, button);
+                break;
+             case 'division':
+                    operations[4].evaluation()
+                    defineOperationLogic('division', 3, button);
+                break;
+             case 'equal': 
+                    
+                    operations[4].evaluation();
+             //  if(operations[0].isPressed === true){
+            //     operator('sum');  
+            //     } else if (operations[1].isPressed === true){
+            //         operator('subtraction');  
+            //     }
+                break;
             }
         })
     })
 };
+
+
 
 
 
@@ -76,40 +81,122 @@ const operations = [{
     id: 'multiplication',
     isPressed: null,
     multiplication(firstValue, secondValue) {
-        return firstValue*secondValue;
+        return firstValue * secondValue;
     }
 }, {
     id: 'division',
     isPressed: null,
     division(firstValue, secondValue) {
-        return firstValue/secondValue;
+        return firstValue / secondValue;
     }
 }, {
     id: 'evaluation',
     isPressed: null,
-    evaluation() {
+    evaluation() {   
+        if (operations[0].isPressed === true) {
+            return operator('sum');
+        } else if (operations[1].isPressed === true) {
+            return operator('subtraction');
+        } else if (operations[2].isPressed === true) {
+            return operator('multiplication');
+        } else if (operations[3].isPressed === true) {
+            return operator('division');
+        } else {
+            console.log('Nothing happened.');
+        }
     }
 },
 ];
-// const operations = {
-//     sum (firstValue, secondValue){
-//         return firstValue+secondValue;
-//     },
-//     subtraction (){},
-//     multiplication (){},
-//     division (){},
-//     evaluation (){
 
-//     },
-// }
 
-function operator (id){
-if(id == 'sum'){
-    firstValue = +firstValue;
-    result = operations[0].sum(result, firstValue);
-    firstValue = '';
-    displayNumber.textContent = `${result}`
-    operations[0].isPressed = false;
-} 
+
+
+
+function defineOperationLogic(operation, operationsArrayId, button) {
+    console.log(`${button.id}`);
+    if (result != '' && firstValue != '') {
+        operator(operation);
+    } else if (result != '' && firstValue == '') {
+        result = result;
+        console.log(`result: ${result}`);
+        firstValue = '';
+        operations[operationsArrayId].isPressed = false;
+    } else {
+        result = +firstValue;
+        console.log(`result: ${result}`);
+        firstValue = '';
+        operations[operationsArrayId].isPressed = true;
+    }
+    operations.filter( pressed => {
+        if(pressed.isPressed === true){
+            console.log(`button pressed: ${pressed.id}`);
+        }
+    })
+}
+
+function operator(id) {
+    switch (id) {
+        case 'sum':
+            firstValue = +firstValue;
+            result = operations[0].sum(result, firstValue);
+            firstValue = '';
+            displayNumber.textContent = `${result}`
+            operations[0].isPressed = false;
+            break;
+        case 'subtraction':
+            firstValue = +firstValue;
+            result = operations[1].subtraction(result, firstValue);
+            firstValue = '';
+            displayNumber.textContent = `${result}`
+            operations[1].isPressed = false;
+            break;
+        case 'multiplication':
+            firstValue = +firstValue;
+            result = operations[2].multiplication(result, firstValue);
+            firstValue = '';
+            displayNumber.textContent = `${result}`
+            operations[2].isPressed = false;
+            break;
+        case 'division':
+            firstValue = +firstValue;
+            result = operations[3].division(result, firstValue);
+            firstValue = '';
+            displayNumber.textContent = `${result}`
+            operations[3].isPressed = false;
+            break;
+    }
 };
+
+
     
+// function handleOperatorButtons (id){
+//     id.forEach( button => {
+//         button.addEventListener('click',
+//         () => {
+//             if (button.id == 'sum'){
+//                 console.log(`${button.id}`);  
+//                 if(result != '' && firstValue != ''){
+//                     operator('sum');     
+//                 } else if(result != '' && firstValue == ''){
+//                     result = result;
+//                     firstValue = '';
+//                     operations[0].isPressed = false;
+//                 } else {
+//                     result = +firstValue;
+//                     firstValue = '';
+//                     operations[0].isPressed = true; 
+//                 }
+//             } else if (button.id == 'equal'){
+//                     if(operations[0].isPressed === true){
+//                     operator('sum');  
+//                     }
+//             } else if (button.value == 'x') {
+//                 operator('x');
+//             } else if (button.value == '/') {
+//                 operator('/');
+//             } else {
+//                 operator('=');
+//             }
+//         })
+//     })
+// };
